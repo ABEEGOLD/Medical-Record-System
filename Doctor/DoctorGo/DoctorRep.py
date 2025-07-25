95ffefb40131b6b37d0f3fa00d747e873fbf737c
ABEEGOLD <abeeutee@gmail.com>
2025-07-25 08:18:44 +0100

    doctor profile

--- Doctor/DoctorGo/DoctorRep.py
+++ Doctor/DoctorGo/DoctorRep.py
@@ -1,3 +1,10 @@
 import datetime
 
-from Doctor import con
+from Doctor.DoctorGo import Contact_details
+
+class Doctor_profile:
+    def __init__(self, doctor_id, date_of_birth:str, email:str, phone_number:int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
