81fb8aa586f8b0cc72c8552e6ad4a1be9c552f23
Macwinner1 <ifechukwuw@gmail.com>
2025-07-26 02:12:37 +0100

    created a construtor to enable class create many object

--- Appointment/appointment.py
+++ Appointment/appointment.py
@@ -1,17 +1,13 @@
-import Appointment
-
-
-class appointment(object):
-    def __init__(self, appointment):
+class Appointment:
+    def __init__(self, appointment_list, appointment_is_booked_status, appointment_id):
         self.appointment_list = []
         self.appointment_is_booked_status = False
-        self.appointment_ID = 0
+        self.appointment_id = 0
 
-
-    def book_appointment(self, Patient, Doctor, Reason, datetime):
-        self.appointment_list.extend([Patient, Doctor, Reason, datetime])
+    def book_appointment(self, patient, doctor, reason, datetime):
+        self.appointment_list.extend([patient, doctor, reason, datetime])
         self.appointment_is_booked_status = True
-        self.appointment_ID += 1
+        self.appointment_id += 1
 
     def get_appointments(self):
         return {self.appointment_list, self.appointment_is_booked_status}
@@ -22,9 +18,10 @@ class appointment(object):
     def get_appointment_list(self):
         return self.appointment_list
 
-    def cancel_appointment(self, appointment_ID):
+    def cancel_appointment(self, appointment_id):
         self.appointment_is_booked_status = False
         for patient in self.appointment_list:
-            if self.appointment_ID == patient:
+            if self.appointment_id == patient:
                 self.appointment_list.remove(patient)
+        return self.appointment_list
 
